357138405fb2de6172d84b7a105143adf81e51df
Hank Barta <hbarta@gmail.com>
2021-11-01 12:05:39 -0500

    adding files produced by VS Code/PlatformIO

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+const int LED = 2; // Assign LED pin i.e: D1 on NodeMCU
+
+void setup() {
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED, HIGH); // turn the LED on
+  delay(1000); // wait for a second
+  digitalWrite(LED, LOW); // turn the LED off
+  delay(1000); // wait for a second
+}
